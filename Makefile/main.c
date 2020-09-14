9a12c42d1ccd68f9920852234f99d4a401d44d39
Felipe Ferreira <felipe@verion.ind.br>
2020-09-14 01:07:30 -0300

    add makefile

---
+++ Makefile/main.c
@@ -0,0 +1,40 @@
+/*
+ * AlgoritmoGenetico.c
+ *
+ * Programa que simula um algoritmo genético.
+ *
+ * Felipe Ferreira Nascimento (Ciência da Computação)
+ * Gabriel Romano Godoi Pereira (Ciência da Computação)
+ * Jaime Mathias de Lara Bueno (Ciência da Computação)
+ * Marcus Vinicius de Souza Olimpio da Silva (Ciência da Computação)
+ * Willy Pestana Filho (Ciência da Computação)
+ *
+ * Disciplina: Inteligência Artificial II
+ *
+ * Professor: Marcio Luiz Piva
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "rotas.h"
+#include <time.h>
+
+int main(void)
+{
+srand(time(NULL));
+	init_mapa();
+	print_mapa();
+	criapop();
+	mostrapop();
+
+	while(TRUE) {
+		avaliapop();
+		reproduzpop();
+		//mostrapop();
+		if(checaparada()) break;
+	}
+	mostrapop();
+	return 0;
+}
---
+++ Makefile/rotas.h
@@ -0,0 +1,58 @@
+#ifndef _ROTA_H
+#define _ROTA_H
+#include <stdbool.h>
+
+#define TRUE 1
+#define FALSE 0
+#define QTGERA 1000
+#define TAMPOP 60
+#define TAMCROMO 30
+#define TAXASEL 0.80
+#define TAXACRUZ 0.90
+#define TAXAMUTA 0.05
+#define LIN 5
+#define COL 6
+//#define INSTALL_DEBUG
+
+typedef struct {
+    int linha;
+    int col;
+    int dado;
+}posicao;
+
+typedef struct {
+    float inf;
+    float sup;
+    float porc;
+    posicao** p;
+}faixas_roleta;
+
+#ifdef INSTALL_DEBUG
+void print_mapa();
+void teste_roleta();
+void teste_gera();
+void debug(posicao p1, posicao p2);
+void print_cromo(int j, int i);
+void debug_pais(posicao** p1, posicao** p2);
+void teste_unit_cromo(posicao** p1, posicao** p2, posicao** f1, posicao** f2, int corte);
+void debug_cruzamento(int j, int l);
+void debug_muta(posicao** p1);
+#endif // INSTALL_DEBUG
+
+void init_mapa();
+void criapop(void);
+void embaralha_alelos(int i);
+void avaliapop(void);
+void ordenar_cromo(int v[][TAMPOP]);
+void pos_cromo(posicao** cromo, int g, int * j);
+void roleta();
+int dis(posicao * inicio, posicao * atual);
+posicao** selecionapais();
+void reproduzpop(void);
+void elitismo();
+bool cruzapais(posicao** pai_1, posicao** pai_2, int* j1, int* j2);
+void mutapais(int j);
+void verifica_repeticoes(int j_pai, int j_filho, int pt_corte_1, int pt_corte_2);
+int checaparada(void);
+void mostrapop(void);
+#endif
